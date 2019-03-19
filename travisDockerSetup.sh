01b44b323c525e9d64eef734e03ec280d0357bc3
Edoardo Federici <18591616+EdoFede@users.noreply.github.com>
2019-03-19 16:26:39 +0100

    Push to Docker hub - FIX 13

--- travisDockerSetup.sh
+++ travisDockerSetup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-sudo service docker stop
-sudo apt-get remove -y docker docker-engine docker.io containerd runc
-sudo curl -fsSL https://get.docker.com |sh
+# sudo service docker stop
+# sudo apt-get remove -y docker docker-engine docker.io containerd runc
+# sudo curl -fsSL https://get.docker.com |sh
 
 sudo mkdir -p /etc/docker
-sudo touch /etc/docker/daemon.json
-echo '{"experimental": true}' |sudo tee /etc/docker/daemon.json
+echo '{ "experimental": true }' | sudo tee /etc/docker/daemon.json
 sudo service docker restart
+cat /etc/docker/daemon.json
+echo '####### Docker version #######'
+docker version
