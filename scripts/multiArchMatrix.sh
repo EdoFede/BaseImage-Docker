2b1ca98bcfe07f95fc2391713a82964dbe1b96b0
EdoFede <18591616+EdoFede@users.noreply.github.com>
2020-01-12 23:37:14 +0100

    Fix for travis build v3

--- scripts/multiArchMatrix.sh
+++ scripts/multiArchMatrix.sh
@@ -3,5 +3,5 @@
 ARCHS=(			amd64	arm32v6	arm32v7	arm64v8	i386	ppc64le)
 DOCKER_ARCHS=(	amd64	arm		arm		arm	386		ppc64le)
 ARCH_VARIANTS=(	NONE	v6		v7		v8	NONE	NONE)
-QEMU_ARCHS=(	NONE	arm		arm		arm	i386	ppc64le)
+QEMU_ARCHS=(	NONE	arm		arm		aarch64	i386	ppc64le)
 TEST_ENABLED=(	1		1		1		1	1		1)
