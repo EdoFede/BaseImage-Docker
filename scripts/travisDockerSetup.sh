2f2fd3b801bd253f245ceb9043c5d521f5a04539
EdoFede <18591616+EdoFede@users.noreply.github.com>
2020-01-19 20:14:01 +0100

    Fix for travis build 12

--- scripts/travisDockerSetup.sh
+++ scripts/travisDockerSetup.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
-# echo ""
-# echo -e "\033[0;34m######### Enabling client experimental features #########\033[0m"
-# mkdir -p $HOME/.docker
-# echo '{ "experimental": "enabled" }' > $HOME/.docker/config.json
-
-# echo ""
-# echo -e "\033[0;34m######### Enabling server experimental features #########\033[0m"
-# echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
-# sudo service docker restart
 
 echo ""
-echo -e "\033[0;34m######### Pulling and starting local registry #########\033[0m"
-docker pull registry:2
-docker run -d -p 5000:5000 --restart always --name registry registry:2
+echo -e "\033[0;34m# OLD Docker engine config file \033[0m"
+cat /etc/docker/daemon.json
 
 echo ""
+echo -e "\033[0;34m# Enabling Docker engine experimental mode \033[0m"
+echo '{"experimental":"enabled"}' | sudo tee /etc/docker/daemon.json
+echo -e "\033[0;34m# Restarting Docker service \033[0m"
+sudo service docker stop
+sudo service docker start
+
+echo ""
+echo -e "\033[0;34m# NEW Docker engine config file \033[0m"
+cat /etc/docker/daemon.json
+
+
+echo ""
+echo -e "\033[0;34m# Enabling Docker client experimental mode \033[0m"
+mkdir -p $HOME/.docker
+echo '{"experimental":"enabled"}' | sudo tee $HOME/.docker/config.json
+
+echo ""
+echo -e "\033[0;34m# Checking docker version \033[0m"
+docker version
+
+echo ""
+echo -e "\033[0;34m# Checking running containers \033[0m"
 docker ps -a
 
-echo ""
-echo -e "\033[0;34m######### Docker version #########\033[0m"
-docker version
+
+# echo ""
+# echo -e "\033[0;34m######### Pulling and starting local registry #########\033[0m"
+# docker pull registry:2
+# docker run -d -p 5000:5000 --restart always --name registry registry:2
