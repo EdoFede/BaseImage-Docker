c14950ff0059b84b215aa7c68f149f42cf119b30
EdoFede <18591616+EdoFede@users.noreply.github.com>
2020-01-19 20:28:02 +0100

    Fix for travis build 13

--- scripts/travisDockerSetup.sh
+++ scripts/travisDockerSetup.sh
@@ -6,10 +6,17 @@ sudo cat /etc/docker/daemon.json
 
 echo ""
 echo -e "\033[0;34m# Enabling Docker engine experimental mode \033[0m"
-echo '{"experimental":"enabled"}' | sudo tee /etc/docker/daemon.json
+echo '{"registry-mirrors": ["https://mirror.gcr.io"], "mtu": 1460, "experimental":"enabled"}' | sudo tee /etc/docker/daemon.json
 echo -e "\033[0;34m# Restarting Docker service \033[0m"
-sudo service docker stop
-sudo service docker start
+sudo service docker restart
+
+
+echo ""
+echo -e "\033[0;34m# Checking Docker restart logs \033[0m"
+echo -e "\033[0;32msystemctl status docker.service\033[0m"
+sudo systemctl status docker.service
+echo -e "\033[0;32mjournalctl -xe\033[0m"
+sudo journalctl -xe
 
 echo ""
 echo -e "\033[0;34m# NEW Docker engine config file \033[0m"
