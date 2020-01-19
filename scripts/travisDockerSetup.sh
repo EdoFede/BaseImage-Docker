fdf604c99d2456c55bf1067e2d2d360dcf7cb36a
EdoFede <18591616+EdoFede@users.noreply.github.com>
2020-01-19 17:47:19 +0100

    Fix for travis build 2

--- scripts/travisDockerSetup.sh
+++ scripts/travisDockerSetup.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-
-mkdir -p $HOME/.docker
-echo '{ "experimental": "enabled" }' > $HOME/.docker/config.json
 echo '####### Docker version #######'
 docker version
+
+echo ""
+echo -e "\033[0;34m######### Enabling client experimental features #########\033[0m"
+mkdir -p $HOME/.docker
+echo '{ "experimental": "enabled" }' > $HOME/.docker/config.json
+
+echo ""
+echo -e "\033[0;34m######### Enabling server experimental features #########\033[0m"
+echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
+sudo service docker restart
+
+echo ""
+echo -e "\033[0;34m######### Pulling and starting local registry #########\033[0m"
+docker pull registry:2
+docker run -d -p 5000:5000 --restart always --name registry registry:2
+
+echo ""
+docker ps -a
